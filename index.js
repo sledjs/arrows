87717695f9cf134a9f5ad6127b7305aad423e0ec
rascada <carftjr@gmail.com>
2016-02-06 14:06:11 +0100

    module init

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ export default class Arrows {
   constructor($core) {
     this.name = 'arrows';
     this.peer = '$';
+  }
 
-    if (this.$) { //TODO better core peer
-      let $arrows = this.$.children;
-      let slides = $core.modules.slides;
+  init(core) {
+    let $arrows = this.$.children;
+    let slides = core.modules.slides;
 
-      [this.$prev, this.$next] = [$arrows[0], $arrows[1]];
+    [this.$prev, this.$next] = [$arrows[0], $arrows[1]];
 
-      this.$next.onclick = slides.next;
-      this.$prev.onclick = slides.prev;
-    }
+    this.$next.onclick = slides.next;
+    this.$prev.onclick = slides.prev;
   }
 }
