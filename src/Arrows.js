c7b7079ac0fbdfda5699dac49f5bf3518df4a1fc
rascada <carftjr@gmail.com>
2016-02-08 02:52:00 +0100

    hide arrow when is useless

--- src/Arrows.js
+++ src/Arrows.js
@@ -1,14 +1,59 @@
 import rx from 'rx';
 
 class Arrows {
+  constructor() {
+    this.hidden = null;
+  }
+
   init(core) {
     let slides = core.modules.slides;
-    let arrow$ = rx.Observable
-      .fromEvent(this.$.children, 'click')
-      .pluck('target', 'previousSibling');
 
-    arrow$.subscribe(forward =>
-      forward ? slides.next() : slides.prev());
+    this.hide(slides.slide);
+    this.hide$(slides)
+      .subscribe(this.hide.bind(this));
+
+    this
+      .show$(slides).subscribe(this.show.bind(this));
+
+    this
+      .arrow$(slides).subscribe(forward =>
+        forward ? slides.next() : slides.prev());
+  }
+
+  hide$(slides) {
+    return rx.Observable
+      .fromEvent(slides, 'change')
+      .filter(n => this.isEnd(n, slides.$$));
+  }
+
+  show$(slides) {
+    return rx.Observable
+      .fromEvent(slides, 'change')
+      .filter(_ => this.hidden !== null)
+      .filter(n => !this.isEnd(n, slides.$$));
+  }
+
+  arrow$(slides) {
+    return rx.Observable
+      .fromEvent(this.$$, 'click')
+      .pluck('target', 'previousSibling');
+  }
+
+  isEnd(n, $slides) {
+    return n === 0 || n === $slides.length - 1;
+  }
+
+  hide(n) {
+    this.show();
+    this.hidden = n ? 1 : n;
+    this.$$[this.hidden].style.display = 'none';
+  }
+
+  show() {
+    if (this.hidden === null) return false;
+
+    this.$$[this.hidden].style.display = null;
+    this.hidden = null;
   }
 }
 
